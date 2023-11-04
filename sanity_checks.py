eb8250688cfbc286b1281c11db5d7df028cdc11d
laurabpaulsen <202005791@post.au.dk>
2023-11-04 17:09:47 +0100

    add threshold

--- sanity_checks.py
+++ sanity_checks.py
@@ -5,8 +5,9 @@ from pathlib import Path
 from utils import load_first_level_models
 from nilearn import plotting
 import matplotlib.pyplot as plt
+from nilearn.glm import threshold_stats_img
 
-def plot_contrast_subject_level(flm, subject_id, ax = None, contrast = "button_press", output_type = "z_score"):
+def plot_contrast_subject_level(flm, subject_id, ax, threshold = False, contrast = "button_press", output_type = "z_score"):
     """
     Calculates and plots the contrast for the given first level model.
 
@@ -16,6 +17,8 @@ def plot_contrast_subject_level(flm, subject_id, ax = None, contrast = "button_p
         First level model.
     subject_id : stre
         Subject ID.
+    threshold : bool, optional
+        if True, a bonferroni corrected threshold is applied. The default is False.
     ax : matplotlib.axes
         Axis to plot on. Defaults to None. If None, a new figure is created.
     contrast : str, optional
@@ -23,12 +26,20 @@ def plot_contrast_subject_level(flm, subject_id, ax = None, contrast = "button_p
     
     Returns
     -------
+    None.
+
     """
 
     contrast_map  = flm.compute_contrast(contrast, output_type = output_type)
 
+    if threshold:
+        contrast_map, threshold = threshold_stats_img(
+            contrast_map, 
+            alpha=0.05, 
+            height_control='bonferroni')
+
     plotting.plot_glass_brain(
-        contrast_map, 
+        contrast_map,
         colorbar=True,
         plot_abs=False, 
         cmap='RdBu',
@@ -36,7 +47,7 @@ def plot_contrast_subject_level(flm, subject_id, ax = None, contrast = "button_p
     
     ax.set_title(f"Subject {subject_id}")
 
-def plot_all_subjects(flms, subject_ids, save_path = None, contrast = "button_press", output_type = "z_score"):
+def plot_all_subjects(flms, subject_ids, threshold = False, save_path = None, contrast = "button_press", output_type = "z_score"):
     """
     Plots the contrast for all subjects in the given list of first level models.
 
@@ -46,6 +57,8 @@ def plot_all_subjects(flms, subject_ids, save_path = None, contrast = "button_pr
         List of first level models.
     subject_ids : list
         List of subject IDs.
+    threshold : bool
+        if True, a bonferroni corrected threshold is applied. The default is False.
     save_path : str, optional
         Path to save the figure to. The default is None.
     contrast : str, optional
@@ -58,7 +71,7 @@ def plot_all_subjects(flms, subject_ids, save_path = None, contrast = "button_pr
     
     for i, (flm, subject_id) in enumerate(zip(flms, subject_ids)):
         ax = axes.flatten()[i]
-        plot_contrast_subject_level(flm, subject_id, ax = ax, contrast = contrast, output_type = output_type)
+        plot_contrast_subject_level(flm, subject_id, ax, threshold, contrast = contrast, output_type = output_type)
 
 
     fig.suptitle(f"Contrast: {contrast}")
@@ -77,4 +90,9 @@ if __name__ in "__main__":
         output_path.mkdir(parents = True)
 
     # plot button press contrast for all subjects
-    plot_all_subjects(flms, subject_ids, save_path = output_path / "button_press_contrast.png", contrast = "button_press", output_type = "z_score")
+    plot_all_subjects(
+        flms, subject_ids, 
+        threshold=True save_path = output_path / "button_press_contrast.png", 
+        contrast = "button_press", 
+        output_type = "z_score"
+        )
