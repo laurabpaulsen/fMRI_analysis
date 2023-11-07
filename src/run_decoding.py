26653931351cd37b957b38b118ef7e89c72376e0
laurabpaulsen <202005791@post.au.dk>
2023-11-07 14:26:10 +0100

    add process mask

--- src/run_decoding.py
+++ src/run_decoding.py
@@ -5,10 +5,10 @@ import pickle
 from pathlib import Path
 import numpy as np
 import nibabel as nib
-from nilearn.image import new_img_like, load_img
+from nilearn.image import get_data, load_img, new_img_like
 from nilearn.decoding import SearchLight
 from sklearn.svm import LinearSVC
-
+from sklearn.model_selection import KFold
 
 def load_contrasts_dir(path:Path):
     """
@@ -77,20 +77,27 @@ if __name__ in "__main__":
 
         # brain mask 
         mask_wb_filename = Path(f"/work/816119/InSpePosNegData/BIDS_2023E/derivatives/sub-{subject}/anat/sub-{subject}_acq-T1sequence_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz")
-        mask_wb = load_img(mask_wb_filename)
+        mask_img = load_img(mask_wb_filename)
 
-        # prep mask for decoding
-        mask_wb = new_img_like(mask_wb, np.ones(mask_wb.shape))
+        process_mask = get_data(mask_img).astype(int)
+        picked_slice = 29
+        process_mask[..., (picked_slice + 1) :] = 0
+        process_mask[..., :picked_slice] = 0
+        process_mask[:, 30:] = 0
+        process_mask_img = new_img_like(mask_img, process_mask)
+
+        cv = KFold(n_splits=10, shuffle=True, random_state=42)
 
 
     
         searchlight = SearchLight(
-            mask_wb,
+            mask_img,
             estimator=LinearSVC(penalty='l2'),
+            process_mask_img=process_mask_img,
             radius=5, 
             n_jobs=-1,
             verbose=10, 
-            cv=10
+            cv=cv
             )
         
 
