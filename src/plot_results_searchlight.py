608917ce6586346934696b54d560b397d218c417
laurabpaulsen <202005791@post.au.dk>
2023-11-09 17:51:05 +0100

    rename

--- src/plot_results_searchlight.py
+++ src/plot_results_searchlight.py
@@ -8,7 +8,14 @@ from nilearn.image import new_img_like, load_img
 if __name__ in "__main__":
     path = Path(__file__).parents[1]
 
-    results_path = path / "results"
+    results_path = path / "data" / "searchlight"
+
+    fig_path = path / "fig" / "searchlight"
+
+    # ensure results path exists
+    if not fig_path.exists():
+        fig_path.mkdir(exist_ok = True)
+
     for subject in ["0116"]:
         # load results (for now just one subject)
         subject_path = results_path / f"sub-{subject}.pkl"
@@ -22,18 +29,18 @@ if __name__ in "__main__":
         #Create an image of the searchlight scores
         searchlight_img = new_img_like(anat_filename, searchlight.scores_)
 
-
+        
         plot_glass_brain(searchlight_img, cmap='jet',colorbar=True, threshold=0.5,
                           title='Image pos vs Image neg (unthresholded)',
                           plot_abs=False)
-        plt.savefig("hej1.png")
+        plt.savefig(fig_path / f"sub-{subject}_searchlight_unthresholded.png")
 
         plot_glass_brain(searchlight_img,cmap='prism',colorbar=True,threshold=0.60,title='pos vs neg (Acc>0.6')
-        plt.savefig("hej2.png")
+        plt.savefig(fig_path / f"sub-{subject}_searchlight_thresholded.png")
 
         plot_stat_map(searchlight_img, cmap='jet',threshold=0.6, cut_coords=[-30,-20,-10,0,10,20,30],
                     display_mode='z',  black_bg=False,
                     title='pos vs neg (Acc>0.6)')
 
-        plt.savefig("hej3.png")
+        plt.savefig(fig_path / f"sub-{subject}_searchlight_statmap.png")
 
