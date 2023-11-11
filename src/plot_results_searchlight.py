f5463e2299208711044595e37e9867a6b8026d80
laurabpaulsen <202005791@post.au.dk>
2023-11-11 12:28:57 +0100

    add plotting function

--- src/plot_results_searchlight.py
+++ src/plot_results_searchlight.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append(str(Path(__file__).parents[1]))
 from utils import chance_level
 
-def plot_searchlight_subject(searchlight_img, subject_id, ax, threshold = False):
+def plot_searchlight_subject(searchlight_img, ax, threshold = False, title = None, plotting_function = plot_glass_brain()):
     """
     Calculates and plots the contrast for the given first level model.
 
@@ -17,12 +17,15 @@ def plot_searchlight_subject(searchlight_img, subject_id, ax, threshold = False)
     ----------
     searchlight_img : 
         The results of the searchlight analysis
-    subject_id : str
-        Subject ID.
     threshold : int
         The chance level threshold
     ax : matplotlib.axes
         Axis to plot on. Defaults to None. If None, a new figure is created.
+    title : str
+        Title of the ax. Defaults to None.
+    plotting_function : function
+        The plotting function to use. Defaults to plot_glass_brain.
+    
 
     Returns
     -------
@@ -30,7 +33,7 @@ def plot_searchlight_subject(searchlight_img, subject_id, ax, threshold = False)
 
     """
 
-    plot_glass_brain(
+    plotting_function(
         searchlight_img,
         colorbar=True,
         plot_abs=False, 
@@ -40,7 +43,9 @@ def plot_searchlight_subject(searchlight_img, subject_id, ax, threshold = False)
         vmax = 1,
         axes=ax)
     
-    ax.set_title(f"Subject {subject_id}")
+    if title:
+        ax.set_title(title)
+
 
 def plot_searchlight_all_subjects(search_light_imgs, subject_ids, threshold = None, save_path = None):
     """
@@ -65,7 +70,7 @@ def plot_searchlight_all_subjects(search_light_imgs, subject_ids, threshold = No
     
     for i, (searchlight_img, subject_id) in enumerate(zip(search_light_imgs, subject_ids)):
         ax = axes.flatten()[i]
-        plot_searchlight_subject(searchlight_img, subject_id, ax, threshold)
+        plot_searchlight_subject(searchlight_img, ax, threshold = threshold, title = f"Subject {subject_id}", plotting_function = plot_stat_map)
 
     # add super title in bold
     fig.suptitle(f"Searchlight", fontweight="bold", fontsize=20)
