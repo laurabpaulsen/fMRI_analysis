0d449615889ea819ee486f02663be140a94f01ad
laurabpaulsen <202005791@post.au.dk>
2023-11-14 13:37:40 +0100

    add classification

--- src/run_decoding_searchlight.py
+++ src/run_decoding_searchlight.py
@@ -71,7 +71,7 @@ if __name__ in "__main__":
         contrasts_pos, contrasts_neg = load_contrasts_dir(contrasts_path / f"sub-{subject}")
 
         # prep contrasts for decoding
-        X, y = prep_X_y(contrasts_pos, contrasts_neg)
+        X_sl, y_sl, x_ = prep_X_y(contrasts_pos, contrasts_neg)
 
         # brain mask 
         mask_wb_filename = Path('/work/816119/InSpePosNegData/BIDS_2023E/derivatives/sub-0116/anat/sub-0116_acq-T1sequence_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz')
@@ -90,10 +90,59 @@ if __name__ in "__main__":
             )
         
 
-        searchlight.fit(X, y)
+        searchlight.fit(X_sl, y_sl)
 
         # save results
         with open(results_path / f"sub-{subject}.pkl", "wb") as f:
             pickle.dump(searchlight, f)
+
+
+        # use most explanatory voxels for classification
+        mask_wb_filename=f'/work/816119/InSpePosNegData/BIDS_2023E/derivatives/sub-{subject}/anat/sub-{subject}_acq-T1sequence_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz'
+        anat_filename=f'/work/816119/InSpePosNegData/BIDS_2023E/derivatives/sub-{subject}/anat/sub-{subject}_acq-T1sequence_run-1_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz'
+        
+        searchlight_img = new_img_like(anat_filename, searchlight.scores_)
+        
+        # get the 500 most predictive voxels
+        perc = 100 * (1-500.0/searchlight.scores_.size)
+
+        # cutoff
+        cutoff = np.percentile(searchlight.scores_, perc)
+
+        # load whole brain mask
+        mask_wb = load_img(mask_wb_filename)
+
+        process_mask2 = mask_wb.get_fdata().astype(int)
+        process_mask2[searchlight.scores_<=cutoff] = 0
+        process_mask2_img = new_img_like(mask_wb, process_mask2)
+
+        masker = NiftiMasker(mask_img=process_mask2_img, standardize=False)
+
+        # plot voxels
+        #plotting.plot_glass_brain effects
+        fig = plotting.plot_glass_brain(searchlight_img, threshold = cutoff)
+        fig.savefig(path / "fig" / f"InSpe_neg_vs_but_searchlightNB_glass_500_{subject}.png", dpi=300)
+
+        # load contrast images
+        contrasts_path = path / "data" / "decoding" / f"sub-{subject}"
+        contrasts_pos, contrasts_neg = load_contrasts_dir(contrasts_path)
+
+
+        # prep contrasts for decoding
+        X, y = prep_X_y(contrasts_pos, contrasts_neg)
+
+        # mask the images
+        masker = NiftiMasker(mask_img=process_mask2_img, standardize = False)
+        fmri_masked = masker.fit_transform(X)
+        print("now running classifier")
+        score_cv_test, scores_perm, pvalue= permutation_test_score(
+            GaussianNB(), fmri_masked, y, cv=10, n_permutations=1000, 
+            n_jobs=-1, random_state=0, verbose=0, scoring=None)
+        
+        # write results to file
+        with open(output_path / f"sub-{subject}_classification.txt", "w") as f:
+            f.write(f"Classification Accuracy: {score_cv_test} (pvalue : {pvalue})")
+
+        print(f"Classification Accuracy: {score_cv_test} (pvalue : {pvalue})")
 
 
