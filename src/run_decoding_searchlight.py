d5db338c3d799ba86913f6f47d1f24a916145c11
laurabpaulsen <202005791@post.au.dk>
2023-11-14 13:44:27 +0100

    add classification using the most predictive model

--- src/run_decoding_searchlight.py
+++ src/run_decoding_searchlight.py
@@ -6,7 +6,12 @@ from pathlib import Path
 from nilearn.image import load_img, concat_imgs
 from nilearn.decoding import SearchLight
 from sklearn.svm import LinearSVC
-from sklearn.model_selection import KFold
+import numpy as np
+from nilearn.input_data import NiftiMasker
+from nilearn.image import new_img_like
+from nilearn import plotting
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import permutation_test_score
 
 
 def load_contrasts_dir(path:Path):
@@ -41,10 +46,29 @@ def prep_X_y(pos_contrasts:list, neg_contrasts:list):
     # concatenate all contrasts and turn into numpy array
     X = pos_contrasts + neg_contrasts
 
-    # concatenate images
-    X = concat_imgs(X)
+    # create list of indices for cross validation
+    indices = np.arange(len(y))
 
-    return X, y
+    # shuffle indices
+    np.random.shuffle(indices)
+
+    # split into train and test set (50/50)
+    train_indices = indices[:len(indices)//2]
+    test_indices = indices[len(indices)//2:]
+
+    # create train and test set
+    X_train = [X[i] for i in train_indices]
+    X_test = [X[i] for i in test_indices]
+    y_train = [y[i] for i in train_indices]
+    y_test = [y[i] for i in test_indices]
+
+    # concatenate images
+    X_train = concat_imgs(X_train)
+    X_test = concat_imgs(X_test)
+
+
+    return X_train, y_train, X_test, y_test
+
 
 
 
@@ -58,6 +82,11 @@ if __name__ in "__main__":
 
     # path to save results
     results_path = path / "data" / "searchlight"
+    output_path = path / "data" / "classification"
+
+    # ensure output path exists
+    if not output_path.exists():
+        output_path.mkdir(exist_ok = True)
 
     # ensure results path exists
     if not results_path.exists():
@@ -71,31 +100,26 @@ if __name__ in "__main__":
         contrasts_pos, contrasts_neg = load_contrasts_dir(contrasts_path / f"sub-{subject}")
 
         # prep contrasts for decoding
-        X_sl, y_sl, x_ = prep_X_y(contrasts_pos, contrasts_neg)
+        X_sl, y_sl, X_cl, y_cl = prep_X_y(contrasts_pos, contrasts_neg)
 
         # brain mask 
         mask_wb_filename = Path('/work/816119/InSpePosNegData/BIDS_2023E/derivatives/sub-0116/anat/sub-0116_acq-T1sequence_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz')
         mask_img = load_img(mask_wb_filename)
 
-        cv = KFold(n_splits=3, shuffle=True, random_state=42)
-
         searchlight = SearchLight(
             mask_img,
             estimator=LinearSVC(penalty='l2'),
-            #process_mask_img=process_mask_img,
             radius=5, 
             n_jobs=-1,
             verbose=10, 
-            cv=cv
+            cv=3,
             )
         
-
         searchlight.fit(X_sl, y_sl)
 
         # save results
         with open(results_path / f"sub-{subject}.pkl", "wb") as f:
             pickle.dump(searchlight, f)
-
 
         # use most explanatory voxels for classification
         mask_wb_filename=f'/work/816119/InSpePosNegData/BIDS_2023E/derivatives/sub-{subject}/anat/sub-{subject}_acq-T1sequence_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz'
@@ -127,16 +151,12 @@ if __name__ in "__main__":
         contrasts_path = path / "data" / "decoding" / f"sub-{subject}"
         contrasts_pos, contrasts_neg = load_contrasts_dir(contrasts_path)
 
-
-        # prep contrasts for decoding
-        X, y = prep_X_y(contrasts_pos, contrasts_neg)
-
         # mask the images
         masker = NiftiMasker(mask_img=process_mask2_img, standardize = False)
-        fmri_masked = masker.fit_transform(X)
+        fmri_masked = masker.fit_transform(X_cl)
         print("now running classifier")
         score_cv_test, scores_perm, pvalue= permutation_test_score(
-            GaussianNB(), fmri_masked, y, cv=10, n_permutations=1000, 
+            GaussianNB(), fmri_masked, y_sl, cv=10, n_permutations=1000, 
             n_jobs=-1, random_state=0, verbose=0, scoring=None)
         
         # write results to file
